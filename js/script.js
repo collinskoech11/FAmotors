dea99fed2ef4f5191cac53680236efc5ffe6c5fc
collinskoech11 <collinskoechck34@gmail.com>
2021-03-27 14:13:21 +0300

    styling update

--- js/script.js
+++ js/script.js
@@ -63,11 +63,11 @@ startSlide();
 
 	/**********************/
 	/*	Client carousel   */
-	/**********************/
-	$('.carousel-client').bxSlider({
-		auto: true,
-	    slideWidth: 234,
-	    minSlides: 2,
-	    maxSlides: 5,
-	    controls: false
-	});
+
+$('.carousel-client').bxSlider({
+	auto: true,
+	slideWidth: 234,
+	minSlides: 2,
+	maxSlides: 5,
+	controls: false
+});
